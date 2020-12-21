53929db9bb969e9e07cf0c699ef21b33d9f09168
Shiva <shiva.gupta@krantitechservices.in>
2020-12-21 09:48:25 +0530

    Added calculator module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'calculator', pathMatch: 'full'},
+  {path: 'calculator', loadChildren: () => import('./calculator/calculator.module').then(m => m.CalculatorModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
